74caec8332afa18d99365d584700d72ac51abb26
gabrielrodrigues <gabri.henri20088@gmail.com>
2026-05-22 13:06:54 -0300

    chore: mudando a cor da logo

--- frontend/components/atoms/Logo.tsx
+++ frontend/components/atoms/Logo.tsx
@@ -3,8 +3,8 @@ import { GraduationCap } from 'lucide-react'
 export default function Logo() {
   return (
     <div className="flex items-center gap-2">
-      <div className="bg-primary p-2 rounded-lg">
-        <GraduationCap className="w-6 h-6 text-primary-foreground" />
+      <div className="p-2 rounded-lg">
+        <GraduationCap className="w-6 h-6 text-foreground dark:text-primary-foreground" />
       </div>
       <span className="text-xl font-bold text-foreground">Minha UEM</span>
     </div>
