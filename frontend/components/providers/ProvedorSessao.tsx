c82a6cc20ab63f9ede0399a3da5b4e8335039edd
gabrielrodrigues <gabri.henri20088@gmail.com>
2026-05-25 16:17:53 -0300

    feat: tirando o refetch

--- frontend/components/providers/ProvedorSessao.tsx
+++ frontend/components/providers/ProvedorSessao.tsx
@@ -4,7 +4,7 @@ import { SessionProvider } from 'next-auth/react'
 
 export default function ProvedorSessao({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider refetchInterval={55 * 60} refetchOnWindowFocus={true}>
+    <SessionProvider refetchInterval={55 * 60} refetchOnWindowFocus={false}>
       {children}
     </SessionProvider>
   )
